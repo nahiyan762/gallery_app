205969cd35e4a29a58d2ec394efb8ff4843efb5a
Mohammad Sultan Al Nahiyan <nahiyan762@users.noreply.github.com>
2022-06-20 21:42:59 +0600

    feat(home): added search box

--- lib/HomeScreen/HomeScreen.dart
+++ lib/HomeScreen/HomeScreen.dart
@@ -3,23 +3,44 @@ import 'package:gallery_app/widget/text_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          alignment: Alignment.centerLeft,
-          height: MediaQuery.of(context).size.height,
-          child: FittedBox(child: titleTextH1(context, "Gallery App")),
+        appBar: AppBar(
+          title: Align(
+              alignment: Alignment.centerLeft,
+              child: titleTextH1(context, "Gallery App")),
         ),
-      ),
-      body: SafeArea(
-        child: Container(
-          color: Colors.black,
-        ),
-      )
-    );
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      prefixIcon:
+                          const Icon(Icons.search_rounded, color: Colors.black),
+                      hintText: 'Search',
+                      contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      isDense: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(0.6))),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Text('hello')
+                        );
+                      },
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
